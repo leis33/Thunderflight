283b7372d3742c0c963fb77e2754c9dcd452a86e
leis33 <l.abdulah@abv.bg>
2024-09-02 16:33:47 +0300

    style: change game height and background color

--- src/utils/Settings.ts
+++ src/utils/Settings.ts
@@ -1,8 +1,8 @@
 class Settings {
     public static GAME_WIDTH: number = 1280;
-    public static GAME_HEIGHT: number = 800;
+    public static GAME_HEIGHT: number = 720;
 
-    public static BACKGROUND_COLOR: string = "#00AA00";
+    public static BACKGROUND_COLOR: string = "#95eda3";
     public static CONTAINER_ID: string = "gameApp";
 }
 
