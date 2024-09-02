44c1eb97a46bdee1cd0b8e02d7e7fc43cb956c71
leis33 <l.abdulah@abv.bg>
2024-09-02 21:17:42 +0300

    feat: add player input

--- src/scenes/Main.ts
+++ src/scenes/Main.ts
@@ -1,6 +1,7 @@
 import * as PIXI from "pixi.js";
 import { Plane } from "../graphics/Plane";
 import { ParallaxBackground } from "../graphics/ParallaxBackground";
+import { Settings } from "../utils/Settings";
 
 class Main extends PIXI.Container {
     private app: PIXI.Application;
@@ -29,6 +30,8 @@ class Main extends PIXI.Container {
         this.plane = new Plane();
         this.plane.x = 300;
         this.plane.y = 300;
+        this.plane.eventMode = "static";
+        this.plane.on("globalpointermove", this.positionPlane, this);
         this.addChild(this.plane);
     }
     // end of create methods
@@ -40,8 +43,31 @@ class Main extends PIXI.Container {
         })
     }
 
+    private positionPlane(event: PIXI.FederatedPointerEvent): void {
+        const coords: PIXI.Point = this.plane.parent.toLocal({ x: event.x, y: event.y });
+            
+        if (coords.y < 0) {
+            coords.y = 0;
+        } else if (coords.y > Settings.GAME_HEIGHT) {
+            coords.y = Settings.GAME_HEIGHT;
+        }
+        
+        if (coords.x < 0) {
+            coords.x = 0;
+        } else if (coords.x > Settings.GAME_WIDTH) {
+            coords.x = Settings.GAME_WIDTH;
+        }
+
+        this.plane.x = coords.x;
+        this.plane.y = coords.y;
+    }
+
     public destroy(): void {
         super.destroy(true);
+
+        if (this.plane) {
+            this.plane.removeAllListeners();
+        }
     }
 }
 
